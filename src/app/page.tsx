993e9d09c7001bc69cf9e0f344799363fc0f6bd2
unknown <dhoouglas@hotmail.com>
2023-10-18 15:58:25 -0300

    feat: add button, card and header components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,8 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div 
-      className="
-        text-red-500 bg-blue-500">
-      Hello World
+    <div>
+      Home
     </div>
   )
 }
