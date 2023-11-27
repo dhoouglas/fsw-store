304d3a12be1ce5c6be8c494371df276d6fa8ef91
unknown <dhoouglas@hotmail.com>
2023-11-27 11:17:16 -0300

    feat: add stripe checkout to cart

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {
     images: {
       domains: ['https://google.com', 'fsw-store.s3.sa-east-1.amazonaws.com'], 
     },
-}
+    experimental: {
+      serverActions: true,
+    },
+};
 
 module.exports = nextConfig
--- src/components/ui/cart.tsx
+++ src/components/ui/cart.tsx
@@ -7,9 +7,23 @@ import { computeProductTotalPrice } from "@/helpers/product";
 import { Separator } from "./separator";
 import { ScrollArea } from "./scroll-area";
 import { Button } from "./button";
+import { createCheckout } from "@/actions/checkout";
+import { loadStripe } from "@stripe/stripe-js";
 
 const Cart = () => {
     const { products, total, subtotal, totalDiscount } = useContext(CartContext);
+
+    const handleFinishPurchaseClick = async () => {
+        const checkout = await createCheckout(products);
+
+        const stripe = await loadStripe(
+            process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY
+        )
+
+        stripe?.redirectToCheckout({
+            sessionId: checkout.id,
+        })
+    }
 
     return ( 
         <div className="flex h-full flex-col gap-8">
@@ -67,7 +81,7 @@ const Cart = () => {
                     <p>R$ {total.toFixed(2)}</p>
                 </div>
 
-                <Button className="mt-7 font-bold uppercase">Finalizar Compra</Button>
+                <Button className="mt-7 font-bold uppercase" onClick={handleFinishPurchaseClick}>Finalizar Compra</Button>
             </div>
         </div>
     );
